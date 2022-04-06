5e38df143ab2b5366b0b89aaee91e46e6f5451f1
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-04-06 07:19:33 -0800

    [checkpoint] cp-1649258372

---
+++ backend/basic-golang/cashier-app/api/api/auth.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type LoginSuccessResponse struct {
+	Username string `json:"username"`
+}
+
+type AuthErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func (api *API) login(w http.ResponseWriter, req *http.Request) {
+	username := req.URL.Query().Get("username")
+	password := req.URL.Query().Get("password")
+	res, err := api.usersRepo.Login(username, password)
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		encoder.Encode(AuthErrorResponse{Error: err.Error()})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(LoginSuccessResponse{Username: *res}) // TODO: replace this
+}
+
+func (api *API) logout(w http.ResponseWriter, req *http.Request) {
+	username := req.URL.Query().Get("username")
+	err := api.usersRepo.Logout(username)
+	encoder := json.NewEncoder(w)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		encoder.Encode(AuthErrorResponse{Error: err.Error()})
+		return
+	}
+
+	encoder.Encode(AuthErrorResponse{Error: username}) // TODO: replace this
+}
