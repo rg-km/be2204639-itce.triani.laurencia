61c5deecc90c4deb81e8c6cd91fda48b0a0a8830
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-04-19 06:12:40 -0800

    streeess aing

--- backend/basic-golang/debugging/assignment/2-reverse-word/main.go
+++ backend/basic-golang/debugging/assignment/2-reverse-word/main.go
@@ -12,15 +12,15 @@ func main() {
 	fmt.Println(res)
 
 	// Try correct answer:
-	// resCorrect := ReverseWordCorrect(arr)
-	// fmt.Println(resCorrect)
+	resCorrect := ReverseWordCorrect(word)
+	fmt.Println(resCorrect)
 }
 
 func ReverseWord(word string) string {
 	n := len(word)
 	temp := []byte(word)
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		left := i
 		right := n - i - 1
 		temp[left], temp[right] = temp[right], temp[left]
@@ -30,5 +30,15 @@ func ReverseWord(word string) string {
 }
 
 func ReverseWordCorrect(word string) string {
-	return 0 // TODO: replace this
+	n := len(word)
+	temp := []byte(word)
+	fmt.Println(temp)
+
+	for i := 0; i < n/2; i++ {
+		left := i
+		right := n - i - 1
+		temp[left], temp[right] = temp[right], temp[left]
+	}
+
+	return string(temp)
 }
