f23d2f89705a84c83311540ccab502d0e20a5693
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-04-20 07:46:34 -0800

    teliti coba

--- backend/data-structure/stack/2-push-cp/main.go
+++ backend/data-structure/stack/2-push-cp/main.go
@@ -1,99 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 )
 
-// Node mewakili node dari linked list
-type Node struct {
-	value int
-	next  *Node
+// Dari inisiasi stack dengan jumlah maksimal elemen 10, cobalah implementasikan operasi push.
+
+var ErrStackOverflow = errors.New("stack overflow")
+
+type Stack struct {
+	// TODO: answer here
+	Top  int
+	Data []int
+	Size int
 }
 
-// LinkedList mewakili linked list
-type LinkedList struct {
-	head *Node
-	len  int
-}
-
-func main() {
-	list := new(LinkedList)
-	list.head = nil
-	list.len = 0
-
-	fmt.Println("Inserting nodes...")
-	list.Insert(12)
-	list.Insert(13)
-	list.Insert(14)
-	list.Insert(15)
-	list.Insert(20)
-
-	linkedlist, err := list.DeleteVal(14)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Printing nodes")
-	for i := 0; i < linkedlist.len; i++ {
-		fmt.Println(linkedlist.GetAt(i).value)
+func NewStack(size int) Stack {
+	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+		Size: size,
 	}
 }
 
-// Insert memasukkan node baru di akhir dari linked list
-func (l *LinkedList) Insert(val int) {
-	n := Node{}
-	n.value = val
-	if l.len == 0 {
-		l.head = &n
-		l.len++
-		return
+func (s *Stack) Push(Elemen int) error {
+	// TODO: answer here
+	if s.Top == s.Size-1 {
+		return ErrStackOverflow
 	}
-	ptr := l.head
-	for i := 0; i < l.len; i++ {
-		if ptr.next == nil {
-			ptr.next = &n
-			l.len++
-			return
-		}
-		ptr = ptr.next
-	}
-}
 
-// DeleteVal menghapus node dengan nilai yang diberikan dari linked list
-func (l *LinkedList) DeleteVal(val int) (LinkedList, error) {
-	ptr := l.head
-	if l.len == 0 {
-		return LinkedList{}, fmt.Errorf("list is empty")
-	}
-	for i := 0; i < l.len; i++ {
-		if ptr.value == val {
-			// TODO: answer here
-			if i == 0 {
-				l.head = ptr.next
-				l.len--
-				return *l, nil
-			}
+	s.Top++
+	s.Data = append(s.Data, Elemen)
 
-			prev := l.GetAt(i - 1)
-			prev.next = ptr.next
-			l.len--
-			return *l, nil
-		}
-		ptr = ptr.next
-	}
-	return LinkedList{}, fmt.Errorf("node not found")
-}
-
-// GetAt mengembalikan node pada posisi yang diberikan dari linked list
-func (l *LinkedList) GetAt(pos int) *Node {
-	ptr := l.head
-	if pos < 0 {
-		return ptr
-	}
-	if pos > (l.len - 1) {
-		return nil
-	}
-	for i := 0; i < pos; i++ {
-		ptr = ptr.next
-	}
-	return ptr
+	return nil
 }
--- backend/data-structure/stack/3-pop-cp/main.go
+++ backend/data-structure/stack/3-pop-cp/main.go
@@ -8,16 +8,41 @@ var ErrStackUnderflow = errors.New("stack underflow")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Data []int
+	Size int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+		Size: size,
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if s.Top == s.Size-1 {
+		return ErrStackUnderflow
+	}
+
+	s.Top++
+	s.Data = append(s.Data, Elemen)
+
+	return nil
 }
 
 func (s *Stack) Pop() (int, error) {
 	// TODO: answer here
+	if s.Top == -1 {
+		return 0, ErrStackUnderflow
+	} else {
+		poppedValue := s.Data[s.Top]
+		s.Top--
+		s.Data = s.Data[:len(s.Data)-1]
+		return poppedValue, nil
+	}
+
 }
