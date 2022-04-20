0064c81b4341dd96b64ff253a9816d259e72ebf7
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-04-20 07:59:02 -0800

    teliti coba

--- backend/data-structure/stack/4-peek-cp/main.go
+++ backend/data-structure/stack/4-peek-cp/main.go
@@ -8,16 +8,37 @@ var ErrEmptyStack = errors.New("stack is empty")
 
 type Stack struct {
 	// TODO: answer here
+	Top int
+	Data []int
+	Size int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+		Size: size,
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if s.Top == s.Size-1 {
+		return ErrEmptyStack
+	}	
+
+	s.Top++
+	s.Data = append(s.Data, Elemen)
+
+	return nil
 }
 
 func (s *Stack) Peek() (int, error) {
 	// TODO: answer here
+	if s.Top == -1 {
+		return 0, errors.New("stack is empty")
+	} else {
+		return s.Data[s.Top], nil
+	}
 }
