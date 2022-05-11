37e94089c1041f6e641ef2983f0d84966d61c5ad
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-05-11 08:28:46 -0800

    avbd

--- backend/golang-http-server/assignment/cashier-app/api/cart.go
+++ backend/golang-http-server/assignment/cashier-app/api/cart.go
@@ -72,6 +72,7 @@ func (api *API) clearCart(w http.ResponseWriter, req *http.Request) {
 	encoder := json.NewEncoder(w)
 	defer func() {
 		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
 			// TODO: answer here
 			encoder.Encode(CartErrorResponse{Error: err.Error()})
 		}
@@ -93,7 +94,7 @@ func (api *API) cartList(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Println(cartItems)
 
-	encoder.Encode(CartListSuccessResponse{CartItems: []repository.CartItem{}}) // TODO: replace this
+	encoder.Encode(CartListSuccessResponse{CartItems: cartItems}) // TODO: replace this
 }
 
 func (api *API) pay(w http.ResponseWriter, req *http.Request) {
@@ -110,6 +111,6 @@ func (api *API) pay(w http.ResponseWriter, req *http.Request) {
 
 	err = api.salesRepo.Add(cartItems)
 
-	encoder.Encode(CartListSuccessResponse{CartItems: []repository.CartItem{}}) // TODO: replace this
+	encoder.Encode(CartListSuccessResponse{CartItems: cartItems}) // TODO: replace this
 
 }
--- backend/golang-http-server/assignment/cashier-app/api/middleware.go
+++ backend/golang-http-server/assignment/cashier-app/api/middleware.go
@@ -27,20 +27,33 @@ func (api *API) AllowOrigin(w http.ResponseWriter, req *http.Request) {
 func (api *API) AuthMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		api.AllowOrigin(w, r)
-		encoder := json.NewEncoder(w)
+		// encoder := json.NewEncoder(w)
 		// Task: 1. Ambil token dari cookie yang dikirim ketika request
 		//       2. return unauthorized ketika token kosong
 		//       3. return bad request ketika field token tidak ada
 
 		// TODO: answer here
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
 		// Task: Ambil value dari cookie token
 
 		// TODO: answer here
 
+		tknStr := c.Value
+
 		// Task: Deklarasi variable claim
 
 		// TODO: answer here
+
+		claims := &Claims{}
 
 		// Task: 1. parse JWT token ke dalam claim
 		//       2. return unauthorized ketika signature invalid
@@ -48,16 +61,68 @@ func (api *API) AuthMiddleWare(next http.Handler) http.Handler {
 		//       4. return unauthorized ketika token sudah tidak valid (biasanya karna token expired)
 
 		// TODO: answer here
+		tknStr = c.Value
+		claims = &Claims{}
+		tkn, err := jwt.ParseWithClaims(tknStr, claims, func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if !tkn.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
 
 		// Task: Validasi
-
-		return next.ServeHTTP(w, r) // TODO: replace this
+		ctx := context.WithValue(r.Context(), "username", claims.Username)
+		next.ServeHTTP(w, r.WithContext(ctx))
+		return
+		// return next.ServeHTTP(w, r) // TODO: replace this
 	})
 }
 
 func (api *API) AdminMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		return next.ServeHTTP(w, r) // TODO: replace this
+		// return next.ServeHTTP(w, r) // TODO: replace this
+		api.AllowOrigin(w, r)
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		tknstr := c.Value
+		claims := &Claims{}
+		tkn, err := jwt.ParseWithClaims(tknstr, claims, func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if !tkn.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		if claims.Role == "admin" {
+			ctx := context.WithValue(r.Context(), "claims", claims)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+		w.WriteHeader(http.StatusForbidden)
 	})
 }
 
