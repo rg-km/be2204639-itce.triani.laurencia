fef1261dda94994bf32d2dc9bc98e9365564bfda
BE2204639-itcetrianilaurencia <laurenciatriani@gmail.com>
2022-06-08 04:08:48 -0800

    abcd

--- backend/database/assigment/cashier-app/repository/cartitems.go
+++ backend/database/assigment/cashier-app/repository/cartitems.go
@@ -15,42 +15,132 @@ func NewCartItemRepository(db *sql.DB) *CartItemRepository {
 }
 
 func (c *CartItemRepository) FetchCartItems() ([]CartItem, error) {
-	var sqlStatement string
+	//var sqlStatement string
 	var cartItems []CartItem
 
 	//TODO: add sql statement here
 	//HINT: join table cart_items and products
+	sqlStatement := `SELECT 
+	cart_items.id,
+	products.category, 
+	cart_items.product_id,   
+	products.product_name, 
+	products.price,
+	cart_items.quantity
+	FROM cart_items 
+	INNER JOIN products 
+	ON cart_items.product_id = products.id`
 
-	return []CartItem{}, nil // TODO: replace this
+	rows, err := c.db.Query(sqlStatement)
+	if err != nil {
+		return cartItems, err
+	}
+
+	for rows.Next() {
+		var cartItem CartItem
+		err := rows.Scan(
+			&cartItem.ID,
+			&cartItem.Category,
+			&cartItem.ProductID,
+			&cartItem.ProductName,
+			&cartItem.Price,
+			&cartItem.Quantity,
+		)
+		if err != nil {
+			return cartItems, err
+		}
+		cartItems = append(cartItems, cartItem)
+	}
+
+	return cartItems, nil
+
 }
 
 func (c *CartItemRepository) FetchCartByProductID(productID int64) (CartItem, error) {
 	var cartItem CartItem
-	var sqlStatement string
+	//var sqlStatement string
 	//TODO : you must fetch the cart by product id
 	//HINT : you can use the where statement
-	return CartItem{}, nil // TODO: replace this
+	sqlStatement := `SELECT 
+	cart_items.id,
+	products.category,
+	cart_items.product_id,  
+	products.product_name, 
+	products.price,
+	cart_items.quantity
+	FROM cart_items 
+	INNER JOIN products 
+	ON cart_items.product_id = products.id
+	WHERE cart_items.product_id = ?`
+
+	row := c.db.QueryRow(sqlStatement, productID)
+	err := row.Scan(
+		&cartItem.ID,
+		&cartItem.Category,
+		&cartItem.ProductID,
+		&cartItem.ProductName,
+		&cartItem.Price,
+		&cartItem.Quantity,
+	)
+	if err != nil {
+		return cartItem, err
+	}
+
+	return cartItem, nil
+
 }
 
 func (c *CartItemRepository) InsertCartItem(cartItem CartItem) error {
 	// TODO: you must insert the cart item
-	return nil // TODO: replace this
+
+	sqlStatement := `INSERT INTO cart_items (product_id, quantity) 
+	VALUES (?, ?)`
+	_, err := c.db.Exec(sqlStatement, cartItem.ProductID, cartItem.Quantity)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
 
 func (c *CartItemRepository) IncrementCartItemQuantity(cartItem CartItem) error {
 	//TODO : you must update the quantity of the cart item
-	return nil // TODO: replace this
+	//HINT : you can use the where statement
+	sqlStatement := `UPDATE cart_items SET quantity = quantity + 1 
+	WHERE product_id = ?`
+	_, err := c.db.Exec(sqlStatement, cartItem.ProductID, cartItem.Quantity)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *CartItemRepository) ResetCartItems() error {
 	//TODO : you must reset the cart items
 	//HINT : you can use the delete statement
-	return nil // TODO: replace this
+	sqlStatement := `DELETE FROM cart_items`
+	_, err := c.db.Exec(sqlStatement)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *CartItemRepository) TotalPrice() (int, error) {
-	var sqlStatement string
+	//var sqlStatement string
 	//TODO : you must calculate the total price of the cart items
 	//HINT : you can use the sum statement
-	return 0, nil // TODO: replace this
+
+	var totalPrice int
+	sqlStatement := `SELECT SUM(products.price * cart_items.quantity) 
+	FROM cart_items 
+	INNER JOIN products ON cart_items.product_id = products.id`
+	err := c.db.QueryRow(sqlStatement).Scan(&totalPrice)
+	if err != nil {
+		return totalPrice, err
+	}
+
+	return totalPrice, nil
 }
--- backend/database/assigment/cashier-app/repository/products.go
+++ backend/database/assigment/cashier-app/repository/products.go
@@ -12,15 +12,43 @@ func NewProductRepository(db *sql.DB) *ProductRepository {
 
 func (p *ProductRepository) FetchProductByID(id int64) (Product, error) {
 	//TODO: You must implement this function fot fetch product by id
-	return Product{}, nil // TODO: replace this
+	sqlStatement := `SELECT * FROM products WHERE id = ?`
+	var product Product
+	row := p.db.QueryRow(sqlStatement, id)
+	err := row.Scan(&product.ID, &product.ProductName, &product.Category, &product.Price, &product.Quantity)
+	if err != nil {
+		return product, err
+	}
+	return product, nil
 }
 
 func (p *ProductRepository) FetchProductByName(productName string) (Product, error) {
 	// TODO: You must implement this function for fetch product by name
-	return Product{}, nil // TODO: replace this
+	sqlStatement := `SELECT * FROM products WHERE product_name = ?`
+	var product Product
+	row := p.db.QueryRow(sqlStatement, productName)
+	err := row.Scan(&product.ID, &product.ProductName, &product.Category, &product.Price, &product.Quantity)
+	if err != nil {
+		return product, err
+	}
+	return product, nil
 }
 
 func (p *ProductRepository) FetchProducts() ([]Product, error) {
 	// TODO: You must implement this function for fetch all products
-	return []Product{}, nil // TODO: replace this
+	sqlStatement := `SELECT * FROM products`
+	var products []Product
+	rows, err := p.db.Query(sqlStatement)
+	if err != nil {
+		return products, err
+	}
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.ID, &product.ProductName, &product.Category, &product.Price, &product.Quantity)
+		if err != nil {
+			return products, err
+		}
+		products = append(products, product)
+	}
+	return products, nil
 }
